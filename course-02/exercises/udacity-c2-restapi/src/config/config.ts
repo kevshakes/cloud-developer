812549cc5b5ee73121048e2067aa17cc00c51a14
Kevin Tuei <kevintuei89@gmail.com>
2022-06-14 05:34:54 +0300

    Changes from Lessons 4 and 5

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramrunnerdev",
+    "password": "udagramrunnerdev",
+    "database": "udagramrunnerdev",
+    "host": "udagramrunnerdev.cxvlmyew863n.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-tuei-dev"
   },
   "jwt": {
     "secret": " "
